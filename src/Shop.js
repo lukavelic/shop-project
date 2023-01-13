21ac96b28f5ae1e448e676754de195e4b2e03ecc
lukavelic <96191586+lukavelic@users.noreply.github.com>
2023-01-13 16:54:06 +0100

    Create Shop component

---
+++ src/Shop.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./layouts/Header";
+import Section from "./layouts/Section";
+
+const Shop = () => {
+    return (
+        <div className="shop">
+            <Header/>
+            <Section className={'shop'}/>
+        </div>
+    )
+}
+
+export default Shop;
